d72b533c0e531f651c1a6ddc97fc727c4106bdc3
Lucas Fogliarini <lucasfogliarini@gmail.com>
2021-03-14 16:47:06 -0300

    Format enum values

--- src/app/movie-session/movie-session.component.ts
+++ src/app/movie-session/movie-session.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MovieSession } from '../models/movie-session.model';
+import { MotionGraphics } from '../models/motion-graphics.enum';
+import { MovieAudio } from '../models/movie-audio.enum';
 
 @Component({
   selector: 'app-movie-session',
@@ -8,6 +10,8 @@ import { MovieSession } from '../models/movie-session.model';
 })
 export class MovieSessionComponent {
   public sessions: MovieSession[];
+  motionGraphic = MotionGraphics;
+  movieAudio = MovieAudio;
 
   constructor(http: HttpClient, @Inject('MOVIE_THEATER_URL') baseUrl: string) {
     http.get<MovieSession[]>(baseUrl + 'moviesessions').subscribe(result => {
